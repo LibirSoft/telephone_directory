e7a02654e3503a8e152bf862d4bfae4b5c37e5b7
LibirSoft <libir@outlook.com>
2021-05-07 06:11:13 +0300

    list component complate

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react'
 
 export default class Form extends Component {
+    state = {
+
+    }
     render() {
         return (
             <div>
-                
+                <form>
+                    <input name={"name"}></input>
+                </form>
             </div>
         )
     }
--- src/Components/List.js
+++ src/Components/List.js
@@ -1,19 +1,33 @@
-import React, { Component } from 'react'
-import "./List.css"
+import React, { Component } from "react";
+import "./List.css";
 
 export default class List extends Component {
-    render() {
-        return (
-            <div className={"listArea"}>
-                <input name={"filter"} id={"filter"} placeholder={" filter by name or phone"} />
-                <ul className={"list"}>
-                    <li>Yuşa Kopuz </li>
-                    <li>G-Hot Özdemir</li>
-                    <li>Özgür Sarıhan</li>
-                    <li>Vatuhan Öksüz</li>
-                    <li>1000Tl Soyisminibilmiyom</li>
-                </ul>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className={"listArea"}>
+        <input
+          name={"filter"}
+          id={"filter"}
+          placeholder={" filter by name or phone"}
+        />
+        <ul className={"list"}>
+          <li>
+            <span className={"name"}>Yuşa Kopuz</span>
+            <span className={"phone"}>12345678912</span>
+            <span className={"clearfix"}></span>
+          </li>
+          <li>
+            <span className={"name"}>G-hot Özdemir</span>
+            <span className={"phone"}>12345678913</span>
+            <span className={"clearfix"}></span>
+          </li>
+          <li>
+            <span className={"name"}>Vatu Oksüz</span>
+            <span className={"phone"}>12345678914</span>
+            <span className={"clearfix"}></span>
+          </li>
+        </ul>
+      </div>
+    );
+  }
 }
