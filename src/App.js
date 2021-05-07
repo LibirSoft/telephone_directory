6fd0f928c0c536c4081752e36efdac96487f90a6
LibirSoft <libir@outlook.com>
2021-05-07 04:46:53 +0300

    some components finished

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import "./App.css";
 import React, { Component } from "react";
 import Contacts from "./Components/Contacts.js"
+import List from "./Components/List.js"
 
 export default class App extends Component {
   state = {
     name: "mehmet",
   };
-  
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-        <Contacts></Contacts>
+          <List />
         </header>
       </div>
     );
--- src/Components/List.js
+++ src/Components/List.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react'
+import "./List.css"
 
 export default class List extends Component {
     render() {
         return (
-            <div>
-                
+            <div className={"listArea"}>
+                <input name={"filter"} id={"filter"} placeholder={" filter by name or phone"} />
+                <ul className={"list"}>
+                    <li>Yuşa Kopuz </li>
+                    <li>G-Hot Özdemir</li>
+                    <li>Özgür Sarıhan</li>
+                    <li>Vatuhan Öksüz</li>
+                    <li>1000Tl Soyisminibilmiyom</li>
+                </ul>
             </div>
         )
     }
